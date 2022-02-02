263f19f051041a0718f292b2fea1981be88446b8
rossidae <rdmw1@st-andrews.ac.uk>
2022-02-02 16:32:31 +0000

    Initial commit for Matlab functions

---
+++ matlab/kgen/K_functions.m
@@ -0,0 +1,17 @@
+clear
+
+% Read in anonymous functions from JSON file
+% All anonymous functions take the same input arguments: an array of
+% coefficients, temperature, salinity and ionic strength
+K_functions_file = jsondecode(fileread("./K_functions.json"));
+K_functions_names = fieldnames(K_functions_file);
+
+% Create an empty map
+K_dictionary = containers.Map();
+
+% Iterate over K's and assign to map
+for K_index = 1:numel(K_functions_names)
+    K_dictionary(string(K_functions_names{K_index}),K_functions_file.(K_functions_names{K_index}))
+end
+
+

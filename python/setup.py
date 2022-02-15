c62283ba925343bc377a5e590203781581f27ae0
Oscar Branson <ob266@cam.ac.uk>
2022-02-15 17:03:41 +0000

    fixed coefficients location in packaging

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(),
-    package_data={'kgen': ['../coefficients/*.json']},
+    package_data={'kgen': ['coefficients/*.json']},
     include_package_data=True,
     zip_safe=True)

ea14abc86a9128b90daee65d356e51f1b7f8a695
Oscar Branson <ob266@cam.ac.uk>
2022-02-15 16:39:30 +0000

    build files for python kgen

---
+++ python/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(),
+    package_data={'kgen': ['../coefficients/*.json']},
+    include_package_data=True,
+    zip_safe=True)

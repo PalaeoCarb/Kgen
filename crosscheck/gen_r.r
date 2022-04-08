e306947ba0376d00ce84e637777a977bed30de52
Oscar Branson <ob266@cam.ac.uk>
2022-04-08 14:59:30 +0100

    added reference to template data file

--- crosscheck/gen_r.r
+++ crosscheck/gen_r.r
@@ -2,4 +2,4 @@
 #  
 # 1. Load test_conditions.csv
 # 2. Run kgen using those inputs
-# 3. Save inputs to ./generated_Ks as R_{calculated, approximated}.csv
+# 3. Save inputs to ./generated_Ks as R_{calculated, approximated}.csv - see ./generated/python_calculated.csv for file format.

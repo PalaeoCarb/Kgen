42e9260308c1a251893bb10b1c721170973582e7
Dennis Mayk <dennis@mayk.eu>
2022-04-13 20:01:07 +0200

    update crosscheck workflow

--- crosscheck/gen_r.r
+++ crosscheck/gen_r.r
@@ -1,9 +1,6 @@
-# Install devtools
-install.packages("devtools")
-
 # Load Kgen
 if("Kgen" %in% (.packages())){
-  detach("package:Kgen", unload=TRUE) 
+  detach("package:Kgen", unload=TRUE)
 }
 devtools::install_local("../r", force=T)
 library("Kgen")
@@ -14,15 +11,15 @@ test_df <- read.csv("test_conditions.csv")
 # 2. Calculate and approximate Ks
 Ks_calc <- calc_Ks(TC = test_df$TempC,
                     S = test_df$Sal,
-                    P = test_df$Pres, 
-                   Mg = test_df$Mg, 
+                    P = test_df$Pres,
+                   Mg = test_df$Mg,
                    Ca = test_df$Ca,
                    MyAMI_calc = T)
 
 Ks_approx <- calc_Ks(TC = test_df$TempC,
                    S = test_df$Sal,
-                   P = test_df$Pres, 
-                   Mg = test_df$Mg, 
+                   P = test_df$Pres,
+                   Mg = test_df$Mg,
                    Ca = test_df$Ca,
                    MyAMI_calc = F)
 

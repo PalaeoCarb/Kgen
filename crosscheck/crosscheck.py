582c4173ce0c5cbc7a392ba0ce1014aacb938910
Oscar Branson <ob266@cam.ac.uk>
2023-09-04 14:32:49 +0800

    added within-lanuage approximated vs. calculated check

--- crosscheck/crosscheck.py
+++ crosscheck/crosscheck.py
@@ -8,6 +8,11 @@ import json
 
 RDIFF_TOLERANCE = 0.0001  # tolrate max 0.01% difference
 
+def compute_relative_difference(ref, test):
+    diff = ref - test
+    rdiff = diff / ref
+    return rdiff
+
 class crosscheck(unittest.TestCase):
     def test_polynomial_coefficients(self):
         with open('../r/inst/coefficients/polynomial_coefficients.json') as f:
@@ -28,15 +33,46 @@ class crosscheck(unittest.TestCase):
         fs = glob('generated_Ks/*.csv')
         
         checks = {}
+        langs = set()
+        methods = set()
         for f in fs:
             lang, method = os.path.basename(f).replace('.csv', '').split('_')
+            langs.add(lang)
+            methods.add(method)
+            
             if method not in checks:
                 checks[method] = {}
             checks[method][lang] = pd.read_csv(f)
+        
+        # compare approximated to calculated within each language
+        approx = checks['approximated']
+        calced = checks['calculated']
+        for lang in langs:
+            test_pass = True
+            msg = ''
+            
+            if lang in approx and lang in calced:
+                print(f'Testing {lang} approximated vs. calculated...')
 
+                rdiff = compute_relative_difference(calced[lang], approx[lang])
+                             
+                maxrdiff = rdiff.abs().max()
+                
+                if np.all(maxrdiff <= RDIFF_TOLERANCE):
+                    print(f'    OK')
+                else:
+                    msg = f'  Max relative difference:'
+                    msg +='\n      ' + maxrdiff[maxrdiff>RDIFF_TOLERANCE].to_string().replace('\n', '\n      ')
+                    test_pass = False
+            
+                with self.subTest(msg=f'{lang}: approximated vs. calculated'):
+                    self.assertTrue(test_pass, msg=f'\n\nKs outside tolerance ({RDIFF_TOLERANCE}):\n{msg}')
+                
+                
+        # make sure the different methods are giving the same result
         for method in checks:
-            langs = list(checks[method].keys())
-            nlangs = len(langs)
+            mlangs = list(checks[method].keys())
+            nlangs = len(mlangs)
             if nlangs < 2:
                 print(f'Only one language ({lang}) uses {method} method - nothing to compare.')
                 continue
@@ -45,14 +81,12 @@ class crosscheck(unittest.TestCase):
             for i,j in zip(*np.triu_indices(nlangs, k=1)):
                 test_pass = True
                 msg = ''
-                ref = checks[method][langs[i]]
-                diff = checks[method][langs[i]] - checks[method][langs[j]]
                 
-                rdiff = diff / ref
-                
+                rdiff = compute_relative_difference(checks[method][mlangs[i]], checks[method][mlangs[j]])
+                             
                 maxrdiff = rdiff.abs().max()
                 
-                print(f'  {langs[i]} vs. {langs[j]}')
+                print(f'  {mlangs[i]} vs. {mlangs[j]}')
                 if np.all(maxrdiff <= RDIFF_TOLERANCE):
                     print(f'    OK')
                 else:
@@ -60,7 +94,7 @@ class crosscheck(unittest.TestCase):
                     msg +='\n      ' + maxrdiff[maxrdiff>RDIFF_TOLERANCE].to_string().replace('\n', '\n      ')
                     test_pass = False
             
-                with self.subTest(msg=f'{method}: {langs[i]} vs. {langs[j]}'):
+                with self.subTest(msg=f'{method}: {mlangs[i]} vs. {mlangs[j]}'):
                     self.assertTrue(test_pass, msg=f'\n\nKs outside tolerance ({RDIFF_TOLERANCE}):\n{msg}')
 
 if __name__ == '__main__':
